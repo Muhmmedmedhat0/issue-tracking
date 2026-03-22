b0cded4c66fdd71c3492842a92da44277da2bdb3
muhmmad_medhat <muhmmedmedhat0@gmail.com>
2026-03-22 20:34:33 +0200

    fix: update className to use correct font variables and add suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.className} ${geistMono.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
